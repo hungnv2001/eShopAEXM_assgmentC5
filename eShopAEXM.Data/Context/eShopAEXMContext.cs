f50e7b9d6eae420c16d50da991c25a737b12f6b1
Hintdeveloper <117146376+Hintdeveloper@users.noreply.github.com>
2024-01-18 19:56:31 +0700

    Create Context

---
+++ eShopAEXM.Data/Context/eShopAEXMContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopAEXM.Data.Context
+{
+    public class eShopAEXMContext : IdentityDbContext
+    {
+        public eShopAEXMContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected eShopAEXMContext()
+        {
+        }
+    }
+}

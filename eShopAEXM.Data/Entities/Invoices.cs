ccebda4ee59e4b7a2850d9ab7c59edc6b5bc9bbc
Hoangzero8688 <140143143+Hoangzero8688@users.noreply.github.com>
2024-01-19 16:23:52 +0700

    test1

--- eShopAEXM.Data/Entities/Invoices.cs
+++ eShopAEXM.Data/Entities/Invoices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace eShopAEXM.Data.Entities
 {
     public class Invoices
     {
+        [Key]
         public Guid Id { get; set; }
         public string ShippingAddress { get; set; }
         public double TotalPrice { get; set; }
